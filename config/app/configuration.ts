95a82eb9506ccff010f54de0a53652769c04b125
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-01-04 23:18:47 +0200

    Fixed NaN port number

--- config/app/configuration.ts
+++ config/app/configuration.ts
@@ -1,9 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
-const port = parseInt(process.env.PORT || process.env.APP_PORT, 10);
-
 export default registerAs('app', () => ({
   name: process.env.APP_NAME,
-  port,
+  port: process.env.PORT || process.env.APP_PORT,
   env: process.env.NODE_ENV
 }));
