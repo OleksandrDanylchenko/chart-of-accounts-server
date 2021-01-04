37150a89030ed1a3a25a4d2b39e3577d510a301e
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-01-04 22:26:13 +0200

    Added env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import envConfig from '../config/envConfig';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [envConfig], cache: true }),
+    TypeOrmModule.forRoot()
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
