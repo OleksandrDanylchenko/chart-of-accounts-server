54a95929d0d3aa4408db611840c3d71014c067d7
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-01-10 12:46:16 +0200

    /feature: Added basic configuration for swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,21 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppConfigService } from './config/app/config.service';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Chart of accounts example')
+    .setDescription('API for Ukrainian chart of accounts')
+    .setVersion('1.0')
+    .addTag('chart-of-accounts')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   const appConfig: AppConfigService = app.get('AppConfigService');
   await app.listen(appConfig.port);
 }
