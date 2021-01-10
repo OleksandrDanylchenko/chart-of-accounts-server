10e825e4ee178e660eb568035441e99eccdb7529
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2021-01-10 12:28:42 +0200

    /feature: Added missing syntheticAccounts property from account interface

--- src/models/accounts/interfaces/account.interface.ts
+++ src/models/accounts/interfaces/account.interface.ts
@@ -1,5 +1,8 @@
+import { ISyntheticAccount } from '../../syntheticAccounts/interfaces/synthetic-account.interface';
+
 export interface IAccount {
   number: number;
   title: string;
   description: string;
+  syntheticAccounts: ISyntheticAccount[];
 }
